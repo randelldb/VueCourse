f3eb82b195450af3cdd51345510dff438b3c0512
Randell b <58916132+randelldb@users.noreply.github.com>
2023-06-05 20:56:30 +0200

    Added debug tools

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@ import MainLayout from '@/Layouts/MainLayout.vue'
 
 
 createInertiaApp({
-    resolve: async (name) => {
+    resolve: name => {
       const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })  
-      const page = await pages[`./Pages/${name}.vue`]()
+      const page = pages[`./Pages/${name}.vue`]
       page.default.layout = page.default.layout || MainLayout
   
       return page
