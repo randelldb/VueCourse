a7824b011aa2a495686c99b858dbcf375f334c46
Randell b <58916132+randelldb@users.noreply.github.com>
2023-06-05 20:43:31 +0200

    init commit

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <Link href="/" >Main Page</Link>&nbsp;
+    <Link href="/hello" >Show Page</Link>
+
+    <div>The page with tine {{timer}}</div>
+    
+    <slot>
+        Default
+    </slot>
+</template>
+
+<script setup>
+    import {Link} from '@inertiajs/vue3'
+    import {ref} from 'vue'
+
+    const timer = ref(0)
+    setInterval(() => timer.value++, 1000)
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Show</h1>
+    <Link href="/">Main Page</Link>
+
+</template>
+
+<script setup>
+    import {Link} from '@inertiajs/vue3'
+</script>
