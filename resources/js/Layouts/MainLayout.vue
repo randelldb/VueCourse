450ff8df175e386a54e21d3499d142ef886bf6c2
RbDev <randell@live.nl>
2023-06-20 21:48:54 +0200

    continur

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,17 +2,22 @@
     <Link href="/" >Main Page</Link>&nbsp;
     <Link href="/hello" >Show Page</Link>
 
-    <div>The page with tine {{timer}}</div>
-    
+    <!-- <div>The page with tine {{timer}}</div> -->
+    <div v-if="page.props.value.flash.success" class="success">
+        {{ page.props.value.flash.success }}
+    </div>
+
     <slot>
         Default
     </slot>
 </template>
 
 <script setup>
-    import {Link} from '@inertiajs/vue3'
-    import {ref} from 'vue'
+    import {Link, usePage} from '@inertiajs/vue3'
 
-    const timer = ref(0)
-    setInterval(() => timer.value++, 1000)
+    const page = usePage()
+    // import {ref} from 'vue'
+
+    // const timer = ref(0)
+    // setInterval(() => timer.value++, 1000)
 </script>
